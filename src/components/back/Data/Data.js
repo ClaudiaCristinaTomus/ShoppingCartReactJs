dfb02bdb74a54ecb265d5e31afdcd49a172f1ea9
ClaudiaCristinaTomus <tomus_clau@yahoo.com>
2023-06-11 16:11:05 +0300

    2nd commit

---
+++ src/components/back/Data/Data.js
@@ -0,0 +1,34 @@
+const Data={
+    productItems:[
+        {
+            id:"1",
+            name:"Beats Headphone",
+            price:999,
+            image:"./assets/1.jpg"
+        },
+        {
+            id:"2",
+            name:"Beats Headphone",
+            price:999,
+            image:"./assets/2.jpg"
+        },
+        {
+            id:"3",
+            name:"Beats Headphone",
+            price:999,
+            image:"./assets/3.jpg"
+        },
+        {
+            id:"4",
+            name:"Beats Headphone",
+            price:999,
+            image:"./assets/2.jpg"
+        },
+        {
+            id:"5",
+            name:"Beats Headphone",
+            price:999,
+            image:"./assets/2.jpg"
+        },
+    ]
+}
